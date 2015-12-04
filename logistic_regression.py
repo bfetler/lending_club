7d206059288a27b64567324c5ee4fb95547d772c
BFetler <bfetler.gmail.com>
2015-12-03 23:42:30 -0500

    add logit model, logistic function

--- logistic_regression.py
+++ logistic_regression.py
@@ -5,15 +5,15 @@ import statsmodels.api as sm
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
-import os
+# import os
 
 loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
 # write out loansData.to_csv(...) only if continuing on from linear_regression.py
 loansData.dropna(inplace=True)
 
-plotdir = 'logistic_plots/'
-if not os.access(plotdir, os.F_OK):
-    os.mkdir(plotdir)
+# plotdir = 'logistic_plots/'
+# if not os.access(plotdir, os.F_OK):
+#     os.mkdir(plotdir)
 
 pat = re.compile('(.*)-(.*)')  # ()'s return two matching fields
 
@@ -28,26 +28,70 @@ loansData['IR_TF'] = loansData['Interest.Rate'].apply(lambda x: 0 if x<12 else 1
 loansData['Loan.Length'] = loansData['Loan.Length'].apply(lambda s: int(s.rstrip(' months')))
 # loansData['FICO.Score'] = loansData['FICO.Range'].map(lambda s: int(s.split('-')[0]))
 loansData['FICO.Score'] = loansData['FICO.Range'].apply(splitSum)
-loansData['Logic.Intercept'] = 1
+loansData['Intercept'] = 1
 
 print 'loansData head\n', loansData[:5]
 # print '\nloansData basic stats\n', loansData.describe()   # print basic stats
 
-# print basic test of IR_TF calculation
-print 'loansData IntRate == 10\n', loansData[loansData['Interest.Rate'] == 10][:5]
-print 'loansData IntRate < 10\n', loansData[loansData['Interest.Rate'] < 10][:5]
-print 'loansData IntRate > 13\n', loansData[loansData['Interest.Rate'] > 13][:5]
+# print test of IR_TF calculation
+# print 'loansData IntRate < 10\n', loansData[loansData['Interest.Rate'] < 10][:5]
+# print 'loansData IntRate > 13\n', loansData[loansData['Interest.Rate'] > 13][:5]
 
 
 # logistic regression example
 # Q: What is the probability of getting a loan from the Lending Club
 #    for $10,000 at an interest rate <= 12% with a FICO score of 750?
-# Model: log(p/1-p) = mx + b
+# Model: log(p/1-p) = mx + b  "logit function"
+#   invert to find p(x) = 1 / (1 + exp(mx + b))  "logistic function"
+# Wiki: The logit function is the quantile function of the
+#       logistic distribution, while the probit is the quantile function
+#       of the normal distribution.  See https://en.wikipedia.org/wiki/Logit
+
+# Probability isn't binary, *assume* p<70% won't get the loan.
+#   if p >= 0.70 then 1, else 0
 # Try to calculate Interest.Rate, dependent variable?
-# Amount.Funded.By.Investors may also be dependent?
+# Loan Amount is Amount.Requested (see linear_regression.py).
+# Amount.Funded.By.Investors is also dependent?
 dep_variables = ['Interest.Rate', 'Amount.Funded.By.Investors']
 indep_variables = ['FICO.Score', 'Amount.Requested', 'Loan.Length', 'Loan.Purpose', 'Debt.To.Income.Ratio', 'State', 'Home.Ownership', 'Monthly.Income', 'Open.CREDIT.Lines', 'Revolving.CREDIT.Balance', 'Inquiries.in.the.Last.6.Months', 'Employment.Length']
+
+# Really what we want is:
+#    -using linear regression eq, plug in values
+#    -calculate probability then p <= 12%
+# interest_rate = b + a1 * FICO.Score + a2 * Amount.Requested
+#               = b + a1 * 750 + a2 * 10000
+print '''interest_rate = b + a1 * FICO.Score + a2 * Amount.Requested
+              = b + a1 * 750 + a2 * 10000'''
+print 'find p(x) = 1 / (1 + exp(a1*x1 + a2*x2 + b))  "logistic function"'
+
+dep_variables = ['IR_TF']
+indep_variables = ['FICO.Score', 'Amount.Requested', 'Intercept']
+# plus Logic.Intercept ?
 print 'Dependent Variable(s):', dep_variables
 print 'Independent Variables:', indep_variables
+
+logit = sm.Logit( loansData['IR_TF'], loansData[indep_variables] )
+result = logit.fit()
+print 'fit coefficients:\n', result.params
+
+# Why do my coefficients have opposite sign of thinkful notes?
+
+def logistic_fn(loanAmount, fico, params):
+#   a1 = -0.0875    # approximate hard-coded values to start
+#   a2 =  0.000174
+#   b  =  60.347
+#   print 'params', # params.__class__,  params  # class Series
+#   print params['FICO.Score'], params['Amount.Requested'], params['Intercept']
+    a1 = params['FICO.Score']
+    a2 = params['Amount.Requested']
+    b  = params['Intercept']
+    p  = 1 / (1 + np.exp( b + a1 * fico + a2 * loanAmount ))
+    return p
+
+print 'logistic values:\n', 10000, 750, logistic_fn(10000, 750, result.params)
+print 10000, 720, logistic_fn(10000, 720, result.params)
+print 10000, 710, logistic_fn(10000, 710, result.params)
+
+print '\nThe probability that we can obtain a loan at less than 12 percent interest for $10000 with a FICO score of 720 is: %.1f percent.' % ( 100 * logistic_fn(10000, 720, result.params) )
 
 
