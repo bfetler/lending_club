6c56aa1b677632c2c31a3d1528e746c2c3a203af
BFetler <bfetler.gmail.com>
2015-12-10 21:07:35 -0500

    add text to plots, add conclusion

--- multivariate.py
+++ multivariate.py
@@ -63,11 +63,11 @@ plt.bar(freq.keys(), freq.values(), width=1)
 # plt.xlabel('Log10 Income')
 plt.title('RENT                ANY           MORTGAGE            OWN')
 plt.ylabel('Frequency')
-# plt.title('RENT  ANY  MORTGAGE  OWN')
 plt.savefig(plotdir+'home_barplot.png')
 
 # log_income is normally distributed, annual_inc is not
 df2.boxplot(by='home_ownership', column='annual_inc')
+plt.text(1, 6500000, 'Annual income not normally distributed, try log')
 plt.savefig(plotdir+'home_income_boxplot.png')
 df2.boxplot(by='home_ownership', column='log_income')
 plt.savefig(plotdir+'home_logincome_boxplot.png')
@@ -98,6 +98,8 @@ plt.savefig(plotdir+'intrate_v_income.png')
 est2 = smf.ols(formula='int_rate ~ log_income * home_type', data=df2).fit()
 print 'interaction:\n', est2.summary()
 print 'interaction params:\n', est2.params
+print "\nConclusion: \nIt's difficult to visually tell the difference in the plot fit lines with or without an interaction term."
+print 'Interaction fit has some p-values > 0.05, is it signiicant?  R-squared is the same.  Only F-statistic and Cond. No. seem different.'
 
 plt.clf()
 plt.scatter(df2.log_income, df2.int_rate, alpha=0.3, c=df2['home_color'], linewidths=0)
@@ -105,15 +107,10 @@ plt.xlabel('Log10 Income')
 plt.ylabel('% Interest Rate')
 plt.title('Fit: Interaction with home_ownership')
 
-# plt.plot(income_linspace, est2.params['Intercept'] + est2.params['home_type'] + est2.params['log_income'] * income_linspace + est2.params['log_income:home_type'] * income_linspace, 'g')
-
 plt.plot(income_linspace, est2.params['Intercept'] + est2.params['home_type'] + est2.params['log_income'] * income_linspace + est2.params['log_income:home_type'] * income_linspace, 'g')
 plt.plot(income_linspace, est2.params['Intercept'] + est2.params['home_type'] + 0 * est2.params['log_income'] * income_linspace + 0 * est2.params['log_income:home_type'] * income_linspace, 'b')
 plt.plot(income_linspace, est2.params['Intercept'] + est2.params['home_type'] + est2.params['log_income'] * income_linspace + 0 * est2.params['log_income:home_type'] * income_linspace, 'r')
+plt.text(4, 2.5, 'green fit line contains all terms')
 
-# plt.plot(income_linspace, est2.params['Intercept'] + est2.params['log_income'] * income_linspace + est2.params['log_income:home_type'] * est2.params['home_type'] * income_linspace, 'r')
-# almost no different between plotted lines?
-
-# plt.show()
 plt.savefig(plotdir+'intrate_v_income_hometype.png')
 
