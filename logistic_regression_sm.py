8ed06f6487c0c318301e262d4ad6edab10c6f2eb
BFetler <bfetler.gmail.com>
2016-03-25 17:24:29 -0400

    clean up sm logistic regression

--- logistic_regression_sm.py
+++ logistic_regression_sm.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 import re
 import os
 
-loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
+# loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
 # write out loansData.to_csv(...) only if continuing on from linear_regression.py
+loansData = pd.read_csv('data/loansData.csv')  # downloaded data
 loansData.dropna(inplace=True)
 
 plotdir = 'logistic_plots/'
@@ -31,16 +32,16 @@ loansData['Loan.Length'] = loansData['Loan.Length'].apply(lambda s: int(s.rstrip
 loansData['FICO.Score'] = loansData['FICO.Range'].apply(splitSum)
 loansData['Intercept'] = 1
 
-print 'loansData head\n', loansData[:5]
+print('loansData head\n', loansData[:5])
 # print '\nloansData basic stats\n', loansData.describe()   # print basic stats
 
 # print test of IR_TF calculation
-# print 'loansData IntRate < 10\n', loansData[loansData['Interest.Rate'] < 10][:5]
-# print 'loansData IntRate > 13\n', loansData[loansData['Interest.Rate'] > 13][:5]
-# print 'loansData IntRate > 13\n', loansData[loansData['Interest.Rate'] > 13][:5]
-print 'loansData IntRate < 10\n', loansData[loansData['Interest.Rate'] < 10][:5]
-print 'loansData FICO > 820\n', loansData[loansData['FICO.Score'] > 820]
-print 'loansData FICO < 650\n', loansData[loansData['FICO.Score'] < 650]
+# print('loansData IntRate < 10\n', loansData[loansData['Interest.Rate'] < 10][:5])
+# print('loansData IntRate > 13\n', loansData[loansData['Interest.Rate'] > 13][:5])
+# print('loansData IntRate > 13\n', loansData[loansData['Interest.Rate'] > 13][:5])
+print('loansData IntRate < 10\n', loansData[loansData['Interest.Rate'] < 10][:5])
+print('loansData FICO > 820\n', loansData[loansData['FICO.Score'] > 820])
+print('loansData FICO < 650\n', loansData[loansData['FICO.Score'] < 650])
 
 
 # logistic regression example
@@ -65,18 +66,18 @@ indep_variables = ['FICO.Score', 'Amount.Requested', 'Loan.Length', 'Loan.Purpos
 #    -calculate probability then p <= 12%
 # interest_rate = b + a1 * FICO.Score + a2 * Amount.Requested
 #               = b + a1 * 750 + a2 * 10000
-print '''interest_rate = b + a1 * FICO.Score + a2 * Amount.Requested
-              = b + a1 * 750 + a2 * 10000'''
-print 'find p(x) = 1 / (1 + exp(a1*x1 + a2*x2 + b))  "logistic function"'
+print('''interest_rate = b + a1 * FICO.Score + a2 * Amount.Requested
+              = b + a1 * 750 + a2 * 10000''')
+print('find p(x) = 1 / (1 + exp(a1*x1 + a2*x2 + b))  "logistic function"')
 
 dep_variables = ['IR_TF']
 indep_variables = ['FICO.Score', 'Amount.Requested', 'Intercept']
-print 'Dependent Variable(s):', dep_variables
-print 'Independent Variables:', indep_variables
+print('Dependent Variable(s):', dep_variables)
+print('Independent Variables:', indep_variables)
 
 logit = sm.Logit( loansData['IR_TF'], loansData[indep_variables] )
 result = logit.fit()
-print 'fit coefficients:\n', result.params
+print('fit coefficients:\n', result.params)
 
 # Why do my coefficients have opposite sign of thinkful notes?
 # Because IR_TF lambda expression is backwards?
@@ -117,18 +118,18 @@ def pred(loanAmount, fico, params):
     msg += 'get the loan for under 12 percent.'
     return msg
 
-print 'logistic values:\nloan  fico probability'
-print 10000, 750, logistic_fn(10000, 750, result.params), pred(10000, 750, result.params)
-print 10000, 720, logistic_fn(10000, 720, result.params), pred(10000, 720, result.params)
-print 10000, 710, logistic_fn(10000, 710, result.params), pred(10000, 710, result.params)
-print 10000, 700, logistic_fn(10000, 700, result.params), pred(10000, 700, result.params)
-print 10000, 690, logistic_fn(10000, 690, result.params), pred(10000, 690, result.params)
+print('logistic values:\nloan  fico probability')
+print(10000, 750, logistic_fn(10000, 750, result.params), pred(10000, 750, result.params))
+print(10000, 720, logistic_fn(10000, 720, result.params), pred(10000, 720, result.params))
+print(10000, 710, logistic_fn(10000, 710, result.params), pred(10000, 710, result.params))
+print(10000, 700, logistic_fn(10000, 700, result.params), pred(10000, 700, result.params))
+print(10000, 690, logistic_fn(10000, 690, result.params), pred(10000, 690, result.params))
 
-print '\nThe probability that we can obtain a loan at less than 12 percent interest for $10000 USD with a FICO score of 720 is: %.1f percent.  It is more likely than not we will get the loan for under 12 percent.' % ( 100 - 100 * logistic_fn(10000, 720, result.params) )
+print('\nThe probability that we can obtain a loan at less than 12 percent interest for $10000 USD with a FICO score of 720 is: %.1f percent.  It is more likely than not we will get the loan for under 12 percent.' % ( 100 - 100 * logistic_fn(10000, 720, result.params) ))
 
 plt.clf()
 fico_array = range(540, 860, 10)
-fico_logit = map(lambda x: logistic_fn(10000, x, result.params), fico_array)
+fico_logit = list(map(lambda x: logistic_fn(10000, x, result.params), fico_array))
 # print 'fico array:', fico_array, fico_logit
 plt.plot(fico_array, fico_logit)
 plt.xlim(550, 850)
@@ -140,14 +141,14 @@ plt.savefig(plotdir+'fico_logistic.png')
 
 plt.clf()
 divvy = 20
-loan_array = map(lambda x: 10 ** (float(x) / divvy), range(2*divvy, 5*divvy))
-loan_logit = map(lambda x: logistic_fn(x, 720, result.params), loan_array)
+loan_array = list(map(lambda x: 10 ** (float(x) / divvy), range(2*divvy, 5*divvy)))
+loan_logit = list(map(lambda x: logistic_fn(x, 720, result.params), loan_array))
 # print 'loan array:', loan_array, loan_logit
 plt.plot(loan_array, loan_logit)
 # plt.xscale('log')
 plt.xlim(0, 40000)
 locs, labels = plt.xticks()
-plt.xticks(locs, map(lambda x: '$'+str(int(x/1000))+'k', locs))
+plt.xticks(locs, list(map(lambda x: '$'+str(int(x/1000))+'k', locs)))
 plt.xlabel('Loan Amount Requested, USD')
 plt.ylabel('Probability : Interest Rate > 12%')
 plt.title('Logistic Plot')
@@ -160,12 +161,12 @@ plt.scatter(loansData['FICO.Score'], loansData['Amount.Requested'], c=loansData[
 plt.xlim(620, 850)
 plt.ylim(0, 40000)
 locs, labels = plt.yticks()
-plt.yticks(locs, map(lambda x: '$'+str(int(x/1000))+'k', locs))
+plt.yticks(locs, list(map(lambda x: '$'+str(int(x/1000))+'k', locs)))
 plt.xlabel('FICO Score')
 plt.ylabel('Loan Amount Requested, USD')
 plt.title('Interest Rates: red > 12%, blue < 12%')
 # plt.legend(['red > 12% interest, blue < 12% interest'])
 plt.savefig(plotdir+'loan_v_fico.png')
 
-print '\nplots created: fico_logistic.png, loan_logistic.png, loan_v_fico.png'
+print('\nplots created: fico_logistic.png, loan_logistic.png, loan_v_fico.png')
 
