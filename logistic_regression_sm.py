19338eae51eaed7a2f7b77c4689c890fa1dd16a2
BFetler <bfetler.gmail.com>
2016-03-31 12:38:55 -0400

    test cutoff for log_reg_sm, refactor methods

--- logistic_regression_sm.py
+++ logistic_regression_sm.py
@@ -55,13 +55,14 @@ def read_data():
     testData = loansData[dsize:]
     loansData = loansData[:dsize]
     
+    # move to test()
     # print test of IR_TF calculation
-    # print('loansData IntRate < 10\n', loansData[loansData['Interest.Rate'] < 10][:5])
-    # print('loansData IntRate > 13\n', loansData[loansData['Interest.Rate'] > 13][:5])
-    # print('loansData IntRate > 13\n', loansData[loansData['Interest.Rate'] > 13][:5])
-    print('loansData IntRate < 10\n', loansData[loansData['Interest.Rate'] < 10][:5])
-    print('loansData FICO > 820\n', loansData[loansData['FICO.Score'] > 820])
-    print('loansData FICO < 650\n', loansData[loansData['FICO.Score'] < 650])
+    # print('loansData IntRate < 10\n', loansData[loansData['Interest.Rate'] < 10][:3])
+    # print('loansData IntRate > 13\n', loansData[loansData['Interest.Rate'] > 13][:3])
+    # print('loansData IntRate > 13\n', loansData[loansData['Interest.Rate'] > 13][:3])
+    print('loansData IntRate < 10\n', loansData[loansData['Interest.Rate'] < 10][:3])
+    print('loansData FICO > 820\n', loansData[loansData['FICO.Score'] > 820][:3])
+    print('loansData FICO < 650\n', loansData[loansData['FICO.Score'] < 650][:3])
     
     return loansData, testData
 
@@ -121,16 +122,16 @@ def do_logit(loansData, indep_variables, print_out=False):
         print('Independent Variables:', indep_variables)
     
     logit = sm.Logit( loansData['IR_TF'], loansData[indep_variables] )
-    result = logit.fit()    # noisy output
+    result = logit.fit(disp=False)    # remove noisy output
     if print_out:
         print('fit coefficients class', result.params.__class__ , '\n', result.params)
         print('result index', result.params.index, '\nresult values', result.params.values)
 
     return result
 
-def fit_logit(loansData, numeric_vars, cutoff=0.5):
+def fit_score_logit(loansData, numeric_vars, cutoff=0.5):
     result = do_logit(loansData, numeric_vars)
-    score, loansData = get_logistic_prob(loansData, result.params, cutoff)
+    score, loansData = calc_score(loansData, result.params, cutoff)
     return score, result, loansData
 
 # Why do coefficients have opposite sign?  IR_TF lambda is backwards?
@@ -232,8 +233,21 @@ def set_plot_predict(plotdir, app, appf, label, indep_vars, full_df):
     pred_y  = full_df['Pred']
     plot_predict(plotdir, app, appf, label, indep_vars, test_df, test_y, pred_y)
 
+def find_cutoff(dframe, vlist):
+    opt_score = 0
+    opt_cutoff = 0.1
+    cutoffs = [0.2, 0.3, 0.4, 0.45, 0.5, 0.55, 0.6, 0.7, 0.8]
+    for cutoff in cutoffs:
+        tr_score, result, dframe = fit_score_logit(dframe, vlist, cutoff)
+        print("cutoff %.2f, score %.5f" % (cutoff, tr_score))
+        if (tr_score > opt_score):
+            opt_score = tr_score
+            opt_cutoff = cutoff
+    
+    print(">>> opt cutoff %.2f, score %.5f, vars=%s" % (opt_cutoff, opt_score, vlist))
+
 # rename do_predict() ?  depends if applied to train or test
-def get_logistic_prob(dframe, params, cutoff=0.5):
+def calc_score(dframe, params, cutoff=0.5):
     dframe['Prob'] = 0
     for par in params.index:
         dframe['Prob'] -= params[par] * dframe[par]
@@ -243,11 +257,12 @@ def get_logistic_prob(dframe, params, cutoff=0.5):
     score = sum(dframe['IR_TF'] == dframe['Pred']) / dframe.shape[0]
     return score, dframe
 
-def get_score(clf, vlist, dframe):
-    tr_score, result, loansData = fit_logit(dframe, vlist, cutoff=0.5)
+def get_tr_score(clf, vlist, dframe):
+    "fit training data, get train score"
+    tr_score, result, loansData = fit_score_logit(dframe, vlist, cutoff=0.5)
     return tr_score, result
 
-def random_opt(clf, varlist, init_list, dframe, score_fn=get_score, print_out=False):
+def random_opt(clf, varlist, init_list, dframe, score_fn=get_tr_score, print_out=False):
     '''Optimize list by randomly adding variables,
        accept if score decreases to find local minimum.'''
 
@@ -274,7 +289,7 @@ def random_opt(clf, varlist, init_list, dframe, score_fn=get_score, print_out=Fa
     # return dict ?
     return score, vlist, result
 
-def run_opt(clf, numeric_vars, dframe, app, appf, plotdir, score_fn=get_score):
+def run_opt(clf, numeric_vars, dframe, app, appf, plotdir, score_fn=get_tr_score):
     '''Run randomized optimization with full list of independent numeric variables.
        Repeat many times to find global minimum.'''
 
@@ -313,38 +328,43 @@ def main():
     
     loansData, testData = read_data()
     plotdir = make_plotdir()
+    
+    # test steps of fit_score_logit
     indep_vars = ['FICO.Score', 'Amount.Requested', 'Intercept']
     result = do_logit(loansData, indep_vars, print_out=True)
     test_results(result)
+    
+    # train score
+    tr_score, loansData = calc_score(loansData, result.params)
+    # test score
+    score, testData = calc_score(testData, result.params)
+    print("testData head\n", testData[:3])
+    print("score 3 vars: train %.5f, test %.5f" % (tr_score, score))
+    
+    set_plot_predict(plotdir, app, appf, "var3_p5", indep_vars, testData)
     
     plot_fico_logit(result, plotdir)
     plot_loan_logit(result, plotdir)
     plot_loan_fico(loansData, result, plotdir)
     
-    # train score
-    tr_score, loansData = get_logistic_prob(loansData, result.params)
-    # test score
-    score, testData = get_logistic_prob(testData, result.params)
-    print("testData head\n", testData[:3])
-    print("score 3 vars: train %.5f, test %.5f" % (tr_score, score))
-    # plot pseudo-predict from train fit data
-#    set_plot_predict(plotdir, app, appf, "var3f_p5", indep_vars, loansData)
-    set_plot_predict(plotdir, app, appf, "var3_p5", indep_vars, testData)
-    
-    print('\nplots created: fico_logistic.png, loan_logistic.png, loan_v_fico.png')
+    # find optimum p-cutoff value
+    find_cutoff(loansData, indep_vars)
 
 # similar processing to sklearn
 #    loans_df, loans_y, test_df, test_y, numeric_vars = load_data(loansData, testData)
     numeric_vars = ['FICO.Score', 'Amount.Requested', 'Intercept', 'Home.Type', 'Revolving.CREDIT.Balance', 'Monthly.Income', 'Open.CREDIT.Lines', 'Debt.To.Income.Ratio', 'Loan.Length', 'Loan.Purpose.Score', 'Amount.Funded.By.Investors', 'Inquiries.in.the.Last.6.Months']
-#    result = do_logit(loansData, numeric_vars, print_out=True)
-#    score, loansData = set_logistic_prob(loansData, result.params)
-    tr_score, result, loansData = fit_logit(loansData, numeric_vars)
-    score, testData = get_logistic_prob(testData, result.params)
+    tr_score, result, loansData = fit_score_logit(loansData, numeric_vars)
+    score, testData = calc_score(testData, result.params)
 #    print("loansData head\n", loansData[:3])
     print("score 11 vars: train %.5f, test %.5f" % (tr_score, score))
+    find_cutoff(loansData, numeric_vars)
     set_plot_predict(plotdir, app, appf, "var11_p5", numeric_vars, testData)
     
     opt_score, opt_list, opt_result = run_opt("lrsm", numeric_vars, loansData, app, appf, plotdir)
+    
+    find_cutoff(loansData, opt_list)
+    set_plot_predict(plotdir, app, appf, "varopt_p5", opt_list, testData)
+
 
 if __name__ == '__main__':
     main()
