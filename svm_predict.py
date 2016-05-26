63b422f39151f96d2704a3ca5fc7ac745c00f6fe
BFetler <bfetler.gmail.com>
2016-05-25 21:40:26 -0400

    update logistic regression

--- svm_predict.py
+++ svm_predict.py
@@ -171,8 +171,8 @@ def do_boxplot(vals, labs, app, xlabel, plotfile):
     else:
         sig = "No significant difference in any parameters (p-value > 0.05)"
     plt.clf()
-#    plt.boxplot(vals, labels=labs)
-    plt.boxplot(vals, labels=labs, showmeans=True)
+    plt.boxplot(vals, labels=labs)
+#   plt.boxplot(vals, labels=labs, showmeans=True)
     # showmeans=True  useful for interp run_opt
     plt.title("High / Low Loan Rate Grid Score Fit by " + app)
     plt.xlabel(xlabel + "\n" + sig)
