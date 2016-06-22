3107fc6616f770b508b0a91b695c9ea0d3527362
BFetler <bfetler.gmail.com>
2016-06-21 21:13:14 -0400

    add lr coef sort

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,6 +2,8 @@
 
 from sklearn.linear_model import LogisticRegression as lr
 from sklearn.grid_search import GridSearchCV
+import numpy as np
+import pandas as pd
 import os
 
 from svm_predict import load_data, do_fit, do_predict, plot_predict, \
@@ -31,6 +33,16 @@ def print_coefs(clf):
     "print logistic regression coefficients"
     print("coefs:", clf.coef_, "intercept", clf.intercept_, "n_iter", clf.n_iter_)
     # traditional coefs in "frequentist" style?
+
+def print_lr_coefs(clf, X_labels):
+    "print logistic regression coefficients"
+    plist = ((lab, val) for lab, val in zip(X_labels, clf.coef_[0]))
+    plist = sorted(plist, key=lambda e: np.abs(e[1]), reverse=True)
+    pser = pd.Series(data = (e[1] for e in plist), index = (e[0] for e in plist))
+#    print('p(x) = 1 / (1 + exp(a1*x1 + a2*x2 + b))  "logistic function"')
+    print("Columns by logistic fit importance (order depends on random split)\n%s" % pser)
+    print("Intercept:", clf.intercept_[0])
+    return plist
 
 def explore_params(loans_X, loans_y, plotdir, app, appf):
     '''Explore fit parameters on training data,
@@ -71,7 +83,23 @@ def main():
     pred_y = do_predict(clf, test_X, test_y, print_out=True)  
     plot_predict(plotdir, app, appf, "allvar", indep_vars, test_df, test_y, pred_y)
     print("columns:", indep_vars)
-    print_coefs(clf)
+#   print_coefs(clf)
+    X_labels = list(loans_df.columns)
+#   print_lr_coefs(clf, X_labels)
+    plist = print_lr_coefs(clf, indep_vars)
+
+# find score using only top5
+    top6 = [p[0] for p in plist[:6]]
+    print("top6:", top6)
+    loans_X = loans_df[top6]
+    test_X = test_df[top6]
+    loans_X, my_scaler = scale_train_data(loans_X, print_out=True)
+    test_X = scale_test_data(my_scaler, test_X)
+    clf = lr()
+    do_fit(clf, loans_X, loans_y, print_out=True)
+    pred_y = do_predict(clf, test_X, test_y, print_out=True)
+    print_lr_coefs(clf, top6)
+    plot_predict(plotdir, app, appf, "top6", top6, test_df, test_y, pred_y)
     
 #    arr = clf.decision_function(loans_df)
 #    print("decision function:", arr.shape, arr)  # shape (1873,)
@@ -107,7 +135,9 @@ def main():
     do_fit(clf, loans_X, loans_y, print_out=True)
     pred_y = do_predict(clf, test_X, test_y, print_out=True)
     print("opt_list columns:", opt_list)
-    print_coefs(clf)
+#   print_coefs(clf)
+#   print_lr_coefs(clf, X_labels)
+    print_lr_coefs(clf, opt_list)
     plot_predict(plotdir, app, appf, "optvar", opt_list, test_df, test_y, pred_y)
     
 #    proba = clf.predict_proba(loans_X)
